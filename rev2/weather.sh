1f38b69ca8919115043c5800c4a1860a1ef6ab25
penguinmoose <67451414+penguinmoose@users.noreply.github.com>
2024-04-21 14:30:52 -0700

    logs go in logs folder (and sleep between retries)

--- rev2/weather.sh
+++ rev2/weather.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd $(dirname $0)
 
+mkdir -p logs
+
 while true
 do
-    unbuffer python3 weather.py > "output$(date +"%Y%m%d-%H%M%S").txt"
+    unbuffer python3 weather.py > "logs/output$(date +"%Y%m%d-%H%M%S").txt"
     echo "exception occured"
+    sleep 5
 done
